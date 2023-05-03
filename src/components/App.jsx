1f7f5ffc6313df2a5d887cc34e7beeea3e22a827
DimiVaksman <vde92@hotmail.com>
2023-05-03 08:39:35 +0200

    hw4 feedback to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,43 +4,6 @@ import { Statistics } from 'components/Statistics/Statistics';
 import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
 import { Section } from './section/section';
 
-// export class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   countTotalFeedback = ({ good, neutral, bad }) => good + neutral + bad;
-
-//   countPositiveFeedbackPercentage = ({ good, neutral, bad }) =>
-//     Math.round((good * 100) / this.countTotalFeedback(this.state));
-
-//   leaveFeedback = evt => {
-//     this.setState({ [evt]: this.state[evt] + 1 });
-//   };
-//   render() {
-//     return (
-//       <Layout>
-//         <Section title="Please leave feedback">
-//           <FeedbackOptions
-//             options={Object.keys(this.state)}
-//             onLeaveFeedback={this.leaveFeedback}
-//           />
-//         </Section>
-//         <Section title="Statistics">
-//           <Statistics
-//             good={this.state.good}
-//             neutral={this.state.neutral}
-//             bad={this.state.bad}
-//             total={this.countTotalFeedback(this.state)}
-//             positiveFeedback={this.countPositiveFeedbackPercentage(this.state)}
-//           />
-//         </Section>
-//       </Layout>
-//     );
-//   }
-// }
 
 export const App = () => {
   const [good, setGood] = useState(0);
